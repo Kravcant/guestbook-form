5c3d922fdb4a0d9832b4add0eac00710e4ecafd7
anthonykravchishin <tonykravc53@gmail.com>
2025-10-24 23:02:05 -0700

    Added form validation for linkedin field and meet dropdown; Changed displays for Other textbox and mailing

--- public/js/script.js
+++ public/js/script.js
@@ -7,19 +7,25 @@ document.getElementById('contactForm').onsubmit = () => {
     // Determines whether form will submit or not
     let isValid = true;
 
-    // Checks if fname is filled. Displays error and prevents form submission is empty
+    // Checks if fname is filled. Displays error and prevents form submission if empty
 	let fname = document.getElementById('fname').value.trim();
 	if (!fname) {
 		document.getElementById('err-fname').style.display = "block";
 		isValid = false;
 	}
 
-    // Checks if lname is filled. Displays error and prevents form submission is empty
+    // Checks if lname is filled. Displays error and prevents form submission if empty
 	let lname = document.getElementById('lname').value.trim();
 	if (!lname) {
 		document.getElementById('err-lname').style.display = "block";
 		isValid = false;
 	}
+
+    let linkedin = document.getElementById('linkedin').value.trim();
+    if (linkedin.length > 0 && !linkedin.includes("https://linkedin.com/in/")) {
+        document.getElementById('err-linkedin').style.display = "block";
+		isValid = false;
+    }
 
     // Checks if email is filled and has correct syntax. Displays error and prevent form submission if email is
     // empty while mailing checkbox is checked, or if the email address does not contain a '@' or '.'
@@ -33,9 +39,33 @@ document.getElementById('contactForm').onsubmit = () => {
 		isValid = false;
     }
 
+    // Checks if a valid selection is picked from meet dropdown. Displays error and prevents form submission if
+    // set to default
+    let meet = document.getElementById('meet').value;
+    if (meet == "default") {
+        document.getElementById('err-meet').style.display = "block";
+		isValid = false;
+    }
+
     // Submits form if all conditions have been met
     return isValid;
 }
+
+document.getElementById('meet').addEventListener('change', function() {
+    if (this.value == "other") {
+        document.getElementById('other-text').style.display = "flex";
+    } else {
+        document.getElementById('other-text').style.display = "none";
+    }
+});
+
+document.getElementById('mailing').addEventListener('change', function() {
+    if (this.checked) {
+        document.querySelector('section').style.display = "flex";
+    } else {
+        document.querySelector('section').style.display = "none";
+    }
+});
 
 // Hides all error messages
 function clearErrors() {
