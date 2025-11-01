68067b7accb8a75fa2e8dcd7765e99772a77bbfc
anthonykravchishin <tonykravc53@gmail.com>
2025-11-01 10:03:31 -0700

    Added routing

--- app.js
+++ app.js
@@ -6,8 +6,40 @@ const PORT = 3002;
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended:true }));
+
+const forms = [];
+
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`)
+});
+
+app.get('/confirm', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirm.html`)
+});
+
+app.get('/admin', (req, res) => {
+    res.send(forms);
+});
+
+app.post('/submit', (req, res) => {
+    const form = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        job: req.body.job,
+        company: req.body.company,
+        linkedin: req.body.linkedin,
+        email: req.body.email,
+        meet: req.body.meet,
+        other: req.body.other,
+        message: req.body.message,
+        mailing: req.body.mailing,
+        format: req.body.method
+    }
+    forms.push(form);
+    console.log(forms);
+
+    res.sendFile(`${import.meta.dirname}/views/confirm.html`);
 });
 
 app.listen(PORT, () => {
